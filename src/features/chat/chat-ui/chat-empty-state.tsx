27ff548032b951fe7921fe7469dc85a1d3b21be1
Mika Lindelä <mika.lindela@kelalab.onmicrosoft.com>
2023-09-06 14:32:19 +0300

    change disclaimer text and a small layout fix

--- src/features/chat/chat-ui/chat-empty-state.tsx
+++ src/features/chat/chat-ui/chat-empty-state.tsx
@@ -37,8 +37,8 @@ export const EmptyState: FC<Prop> = (props) => {
   };
 
   return (
-    <div className="grid grid-cols-5 w-full items-center container mx-auto max-w-3xl justify-center h-full lg:gap-9">
-      <div className="col-span-5 gap-5 flex flex-col flex-1">
+    <div className="grid grid-cols-5 w-full container mx-auto max-w-3xl justify-center h-full overflow-auto pb-24">
+      <div className="col-span-5 gap-5 mt-5 flex flex-col flex-1">
         <Typography variant="h4" className="text-primary">
           Hello!
         </Typography>
