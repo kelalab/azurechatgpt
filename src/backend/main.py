6fff402dc50a2a5c1a62a92fe3e8e07f7ea8bf31
Mika Lindelä <mika.lindela@kelalab.onmicrosoft.com>
2023-11-02 11:38:36 +0200

    add a build process and built files to backend

--- src/backend/main.py
+++ src/backend/main.py
@@ -1,11 +1,14 @@
 from fastapi import FastAPI, UploadFile
+from fastapi.staticfiles import StaticFiles
 from chat_completion import process_input_with_retrieval, get_completion_from_messages
 from add_document.add_document import AddDocument
+import uvicorn
+
 app = FastAPI()
 
-@app.get('/')
-async def root():
-    return {'message': 'Hello World'}
+#@app.get('/')
+#async def root():
+#    return {'message': 'Hello World'}
 
 @app.post('/message')
 async def post_message(benefit:str, message: str):
@@ -26,3 +29,8 @@ async def add_document(benefit: str, file: UploadFile):
     content = await file.read()
     ad = AddDocument(file.filename, content)
     return ad.generate_embeddings(benefit)
+
+app.mount("/", StaticFiles(directory="static", html="true"), name="static")
+
+if __name__ == "__main__":
+    uvicorn.run("main:app", host="127.0.0.1", port=8000, log_level="info")
