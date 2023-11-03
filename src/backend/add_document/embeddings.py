df07ec2e624d30fb7e2a5184015c486287b1203e
Mika Lindelä <mika.lindela@kelalab.onmicrosoft.com>
2023-11-03 16:01:13 +0200

    fix env imports

--- src/backend/add_document/embeddings.py
+++ src/backend/add_document/embeddings.py
@@ -1,14 +1,14 @@
 import time
 import openai
 import openai.error
-import env
+from constants import AZURE_OPENAI_API_INSTANCE_NAME, AZURE_OPENAI_API_VERSION, AZURE_OPENAI_API_KEY
 
 class Embeddings:
    def __init__(self):
-      openai.api_key = env.AZURE_OPENAI_API_KEY
-      openai.api_base = 'https://' + env.AZURE_OPENAI_API_INSTANCE_NAME + '.openai.azure.com' # your endpoint should look like the following https://YOUR_RESOURCE_NAME.openai.azure.com/
+      openai.api_key = AZURE_OPENAI_API_KEY
+      openai.api_base = 'https://' + AZURE_OPENAI_API_INSTANCE_NAME + '.openai.azure.com' # your endpoint should look like the following https://YOUR_RESOURCE_NAME.openai.azure.com/
       openai.api_type = 'azure'
-      openai.api_version = env.AZURE_OPENAI_API_VERSION
+      openai.api_version = AZURE_OPENAI_API_VERSION
 
    def embed(self, text: str, model='text-embedding-ada-002'):
       text = text.replace('\n', ' ')
