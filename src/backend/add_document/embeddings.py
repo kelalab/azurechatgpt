10247d507fad78eb8ea63cc3269f84894802f168
Mika Lindelä <mika.lindela@kelalab.onmicrosoft.com>
2023-11-13 09:38:15 +0200

    more bugfixes

--- src/backend/add_document/embeddings.py
+++ src/backend/add_document/embeddings.py
@@ -1,22 +1,25 @@
 import time
-import open_ai
+from open_ai.open_ai import OpenAi 
 import openai.error
+#import sys
+#sys.path.append("..")
 
 from model.constants import *
 
 class Embeddings:
    def __init__(self):
-      open_ai.api_key = AZURE_OPENAI_API_KEY
-      open_ai.api_base = 'https://' + AZURE_OPENAI_API_INSTANCE_NAME + '.openai.azure.com' # your endpoint should look like the following https://YOUR_RESOURCE_NAME.openai.azure.com/
-      open_ai.api_type = 'azure'
-      open_ai.api_version = AZURE_OPENAI_API_VERSION
+      pass
+      #open_ai.api_key = AZURE_OPENAI_API_KEY
+      #open_ai.api_base = 'https://' + AZURE_OPENAI_API_INSTANCE_NAME + '.openai.azure.com' # your endpoint should look like the following https://YOUR_RESOURCE_NAME.openai.azure.com/
+      #open_ai.api_type = 'azure'
+      #open_ai.api_version = AZURE_OPENAI_API_VERSION
 
    def embed(self, text: str, model='text-embedding-ada-002'):
       text = text.replace('\n', ' ')
       while True:
          try:
             #embedding = open_ai.Embedding.create(input = [text], model=model, deployment_id=model)
-            embedding = open_ai.get_embedding(text)
+            embedding = OpenAi().get_embedding(text = text)
             break
          except openai.error.RateLimitError as e:
             print('retrying...', e)
